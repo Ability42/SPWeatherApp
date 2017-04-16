e38a411cf4838309e224bd1e763dca7d75082f3e
Stepan Paholyk <stepanpaholyk@gmail.com>
2017-04-16 19:36:00 +0300

    Add weather manager prototype,by now use array of mock objects

--- SPWeatherApp/WeatherManager.swift
+++ SPWeatherApp/WeatherManager.swift
@@ -6,8 +6,28 @@
 //  Copyright © 2017 Stepan Paholyk. All rights reserved.
 //
 
-import UIKit
+import Foundation
 
-class WeatherManager: NSObject {
-
+class WeatherManager {
+    
+    
+    func getWeather(with completion: @escaping ([Weather]) -> Void) {
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.75) { 
+            completion(self.mockResponse())
+        }
+    }
+    
+    private func mockResponse() -> [Weather] {
+        
+        var array = [Weather]()
+        
+        array.append(Weather(city: "Kiev", temperatureFahrenheit: 89.7, windDirection: .West, rainfall: .Cloudly, windSpeed: 3.2))
+        array.append(Weather(city: "Rome", temperatureFahrenheit: 119.7, windDirection: .East, rainfall: .Sunny, windSpeed: 1.1))
+        array.append(Weather(city: "London", temperatureFahrenheit: 59.7, windDirection: .North, rainfall: .Stormly, windSpeed: 8.4))
+        
+        return array
+    }
+    
+    
 }
